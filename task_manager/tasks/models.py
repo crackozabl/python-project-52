d87da513ea173ba7bf58ed4d8dc19555ab894ee0
ao <noreply@github.com>
2024-01-07 02:39:33 +0300

    FIX: tasks locale and forms

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -2,18 +2,19 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from task_manager.statuses.models import Status
 from task_manager.labels.models import Label
+from django.utils.translation import gettext as _
 
 
 class Task(models.Model):
-    name = models.CharField(max_length=200, unique=True)
-    description = models.TextField()
+    name = models.CharField(max_length=200, unique=True, verbose_name=_('Name'))
+    description = models.TextField(verbose_name=_('Description'))
     created_at = models.DateTimeField(auto_now_add=True)
 
-    status = models.ForeignKey(Status, on_delete=models.PROTECT, related_name='status')
+    status = models.ForeignKey(Status, on_delete=models.PROTECT, related_name='status', verbose_name=_('Status'))
     author = models.ForeignKey(get_user_model(), on_delete=models.PROTECT, related_name='author')
-    assignee = models.ForeignKey(get_user_model(), on_delete=models.PROTECT, related_name='assignee')
+    assignee = models.ForeignKey(get_user_model(), on_delete=models.PROTECT, related_name='assignee', verbose_name=_('Assignee'))
 
-    labels = models.ManyToManyField(Label, related_name='labels')
+    labels = models.ManyToManyField(Label, related_name='labels', verbose_name=_('Labels'))
 
     def __str__(self):
         return self.name
