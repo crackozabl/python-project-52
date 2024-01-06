2528b635be29815d054d62d8f93edf3ec0a86353
ao <noreply@github.com>
2024-01-06 22:12:09 +0300

    FIX: label_suffix none

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -3,6 +3,12 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class UserRegistrationForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('label_suffix', '')
+        super().__init__(*args, **kwargs)
+        self.fields['password1'].help_text = None
+        self.fields['password2'].help_text = None
+
     class Meta:
         model = get_user_model()
         fields = [
