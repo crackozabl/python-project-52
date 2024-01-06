da3d31fe842a981a633d494a3b7b18c455e0d0b2
ao <noreply@github.com>
2024-01-06 22:55:21 +0300

    FIX: login success message

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.utils.translation import gettext as _
 
 
 def index(request):
@@ -11,8 +13,9 @@ class IndexView(TemplateView):
     template_name = 'index.html'
 
 
-class UserLoginView(LoginView):
+class UserLoginView(SuccessMessageMixin, LoginView):
     template_name = 'login.html'
+    success_message = _('You are logged in.')
 
 
 class UserLogoutView(LogoutView):
