6c83f90b6104af3a56ba0d56ed4e62a10d0cc249
ao <noreply@github.com>
2024-01-06 20:00:31 +0300

    TEST: checks

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -94,9 +94,7 @@ DATABASES = {
     }
 }
 
-
 DATABASES['default'].update(dj_database_url.config(conn_max_age=600))
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
