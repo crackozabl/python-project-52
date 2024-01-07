c0f8a5f59d91df9bb89a38e1e7f3886870c11710
ao <noreply@github.com>
2024-01-08 01:55:32 +0300

    FIX: add allowed hosts render.com

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv('SECRET_KEY') or 'secret-key'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['webserver', '127.0.0.1']
+ALLOWED_HOSTS = ['webserver', '127.0.0.1', 'task-manager-31337.onrender.com']
 
 
 # Application definition
